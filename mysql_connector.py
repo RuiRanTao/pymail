d94569e83d112f16349185cd2bfed5ed4386a602
RuiRanTao <taowuqun@126.com>
2019-05-21 12:53:47 +0800

    first commit

---
+++ mysql_connector.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",  # 数据库主机地址
+    user="root",  # 数据库用户名
+    passwd="990526", # 数据库密码
+    database="test"
+)
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM MyGuests WHERE id >0")
+
+myresult = mycursor.fetchall()  # fetchall() 获取所有记录
+
+for x in myresult:
+    print(x)
